6b5a9bcfeb294807da91ab0cbcd1380013ccd762
SkyJumper409x <alexander.schaaf.jr@icloud.com>
2025-10-22 14:55:00 +0200

    hi

--- custom_components/skyjumper409_stuff/__init__.py
+++ custom_components/skyjumper409_stuff/__init__.py
@@ -3,28 +3,29 @@ import sys
 import logging
 
 _LOGGER = logging.getLogger(__name__)
-
 log = _LOGGER.warning
 
-log(__name__)
-log(os.execv)
-log(os.execl)
-log(sys.argv)
-log(os.path.abspath('.'))
-log(os.path.abspath('__init__.py'))
+async def async_setup(hass, config, discovery_info=None):
+    log(__name__)
+    log(hass, config, discovery_info)
+    log(os.execv)
+    log(os.execl)
+    log(sys.argv)
+    log(os.path.abspath('.'))
+    log(os.path.abspath('__init__.py'))
 
-import urllib.request as req
-try:
-    res = req.urlopen('https://ms.yugipedia.com//thumb/8/83/MadHacker-BACH-EN-C-1E.png/300px-MadHacker-BACH-EN-C-1E.png').read()
-    def writeresto(file: str):
-        with open(file, 'wb') as f:
-            f.write(res)
-        log(f'wrote data to {file}')
-    writeresto('madhacker_1.png')
-    writeresto('../madhacker_2.png')
-    writeresto('/homeassistant/custom_components/madhacker_3.png')
-    writeresto('/homeassistant/custom_components/alexa_media/madhacker4.png')
-except Exception as ex:
-    _LOGGER.warning(f'ex: {ex}')
-
+    import urllib.request as req
+    try:
+        res = req.urlopen('https://ms.yugipedia.com//thumb/8/83/MadHacker-BACH-EN-C-1E.png/300px-MadHacker-BACH-EN-C-1E.png').read()
+        def writeresto(file: str):
+            with open(file, 'wb') as f:
+                f.write(res)
+            log(f'wrote data to {file}')
+        writeresto('madhacker_1.png')
+        writeresto('../madhacker_2.png')
+        writeresto('/homeassistant/custom_components/madhacker_3.png')
+        writeresto('/homeassistant/custom_components/alexa_media/madhacker4.png')
+    except Exception as ex:
+        _LOGGER.warning(f'ex: {ex}')
+async_setup(None, None)
 
